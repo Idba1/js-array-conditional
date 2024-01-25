9bd4013f6d7d26291bbcd65c67384516f275f4e7
Idba1 <moniraislamself@gmail.com>
2024-01-26 05:32:08 +0600

    array.js

---
+++ array.js
@@ -0,0 +1,14 @@
+// Integer array
+var friendAge = [29, 60, 45, 58, 18, 34];
+console.log(friendAge);
+
+
+//Char array
+var vowel = ['a', 'e', 'i', 'o', 'u'];
+console.log(vowel);
+
+
+
+//string array
+var friendName = ['arnob', 'emon', 'istiaq', 'oni', 'udoy'];
+console.log(friendName);
