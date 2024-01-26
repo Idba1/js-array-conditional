2d50a27358c80c7fa3cb15751318e77107321540
Idba1 <moniraislamself@gmail.com>
2024-01-26 17:31:06 +0600

    multiple conditional code

--- multiple-condition.js
+++ multiple-condition.js
@@ -20,3 +20,43 @@ if(isGraduated === true || salary > 50000){
 else{
     console.log('try again');
 }
+
+
+
+var isGraduated = true;
+var salary = 55000;
+var car = 0;
+if(isGraduated === true && salary > 50000 && car >= 1){
+    console.log('congretulations');
+}
+else{
+    console.log('try again');
+}
+
+
+
+
+
+
+var isGraduated = true;
+var salary = 55000;
+var car = 1;
+if(isGraduated === true && salary > 50000 && car >= 1){
+    console.log('congretulations');
+}
+else{
+    console.log('try again');
+}
+
+
+
+
+var isGraduated = true;
+var salary = 55000;
+var car = 0;
+if(isGraduated === true && salary > 50000 || car >= 1){
+    console.log('congretulations');
+}
+else{
+    console.log('try again');
+}
