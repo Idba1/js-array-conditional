d25725c28155e6a88933c8758d677be951231f25
Idba1 <moniraislamself@gmail.com>
2024-01-26 16:42:21 +0600

    variable compare and multiple compare

--- comparison.js
+++ comparison.js
@@ -1,13 +1,15 @@
-// console.log(5 < 7);
-// console.log(5 > 7);
-// console.log(5 == 7);
-// console.log(5 != 7);
-// console.log(5 >= 7);
-// console.log(7 <= 7);
+console.log(5 < 7);
+console.log(5 > 7);
+console.log(5 == 7);
+console.log(5 != 7);
+console.log(5 >= 7);
+console.log(7 <= 7);
 
 
 
 
+
+// variable compare
 var num1 = 9;
 var num2 = 4;
 console.log(num1 > num2);
@@ -16,3 +18,8 @@ console.log(num1 === num2);
 console.log(num1 !== num2);
 console.log(num1 >= num2);
 console.log(num1 <= num2);
+
+
+// Multiple compare
+// mone1 > money2 && result1 > result2 && job1 > job2;
+// mone1 > money2 || result1 > result2 || job1 > job2;
