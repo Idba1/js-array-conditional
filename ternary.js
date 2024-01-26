bf1a4d866c35889c88b11c8f30c481f96f0780eb
Idba1 <moniraislamself@gmail.com>
2024-01-27 00:44:56 +0600

    simple if else & simple ternary

--- ternary.js
+++ ternary.js
@@ -4,5 +4,21 @@
  */
 
 
-var age= 23;
-(age >= 18) ? console.log('Vote dio') : console.log('Boyos hoyni')
+// var age= 23;
+// (age >= 18) ? console.log('Vote dio') : console.log('Boyos hoyni')
+
+
+// // Simple If Else
+let price = 500;
+const isLeader = true;
+if(isLeader === true){
+    console.log(0);
+}
+else{
+    console.log(price +100);
+}
+
+
+
+// tarnary
+price = isLeader === false? console.log(0) : console.log(price +100)
