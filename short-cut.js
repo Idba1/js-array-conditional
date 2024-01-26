2e16016fc1d36a4b3a53132c91e34f955a43997b
Idba1 <moniraislamself@gmail.com>
2024-01-27 01:47:41 +0600

    check boolean false value just use logical not sign

--- short-cut.js
+++ short-cut.js
@@ -1,7 +1,21 @@
+
 const isLeader = true;
+// if(isLeader === true)
 if(isLeader){
     console.log('if leader is coming, you can say WELCOME Leader');
 }
 else{
     console.log('if you are a Amjonota , you are not allow this room');
+}
+
+
+
+
+// if(passed === false)
+const passed = false;
+if(!passed){
+    console.log('Retake');
+}
+else{
+    console.log('waiver');
 }
