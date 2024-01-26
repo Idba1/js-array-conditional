d09cdace22fdea17a476ddaee6954b2d4b229a1f
Idba1 <moniraislamself@gmail.com>
2024-01-27 01:37:33 +0600

    short-cut

---
+++ short-cut.js
@@ -0,0 +1,7 @@
+const isLeader = true;
+if(isLeader){
+    console.log('if leader is coming, you can say WELCOME Leader');
+}
+else{
+    console.log('if you are a Amjonota , you are not allow this room');
+}
