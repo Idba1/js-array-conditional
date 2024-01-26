db46c81946d147cbbfe1a9a79eecebb9cffc1d8c
Idba1 <moniraislamself@gmail.com>
2024-01-26 16:37:01 +0600

    comparison

--- comparison.js
+++ comparison.js
@@ -1,2 +1,18 @@
-console.log(5 < 7);
-console.log(5 > 7);
+// console.log(5 < 7);
+// console.log(5 > 7);
+// console.log(5 == 7);
+// console.log(5 != 7);
+// console.log(5 >= 7);
+// console.log(7 <= 7);
+
+
+
+
+var num1 = 9;
+var num2 = 4;
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
