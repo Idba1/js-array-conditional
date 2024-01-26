df873edca832f106a46a0d64d72625de2b9416c8
Idba1 <moniraislamself@gmail.com>
2024-01-26 23:56:18 +0600

    nested loop

--- nested-condition.js
+++ nested-condition.js
@@ -1,16 +1,40 @@
-var math = true;
-var geometry = false;
-var  straighline = true;
-if (math = true){
-    console.log('math is true');
-            if (geometry = true){
-                console.log('geometry is true');
-            }
-            else if{
-                console.log('geometry is false');
-            }
+// var math = true;
+// var geometry = false;
+// var  straighline = true;
+// if (math = true){
+//     console.log('math is true');
+//             if (geometry = true){
+//                 console.log('geometry is true');
+//             }
+//             else if{
+//                 console.log('geometry is false');
+//             }
    
-   else{
-    console.log('math is false');
-   }
+//    else{
+//     console.log('math is false');
+//    }
+// }
+
+
+
+
+
+
+
+const money = 100;
+if (money > 300 ){
+    console.log('Ami Borolox');
+}
+else{
+    if(money >= 100){
+        console.log('Ami Middle class');  
+    }
+    else{
+        if(0 < money){
+            console.log('Ami Gorib Class'); 
+        }
+        else{
+            console.log('Ami Nai class'); 
+        }
+    }
 }
