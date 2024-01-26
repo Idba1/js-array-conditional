cd493bcb5e6d8cfff415013ef7a278b99a13c25e
Idba1 <moniraislamself@gmail.com>
2024-01-26 17:26:33 +0600

    add otherwise condition

--- multiple-condition.js
+++ multiple-condition.js
@@ -6,3 +6,17 @@ if(isGraduated === true && salary > 50000){
 else{
     console.log('try again');
 }
+
+
+
+
+
+
+var isGraduated = true;
+var salary = 25000;
+if(isGraduated === true || salary > 50000){
+    console.log('congretulations');
+}
+else{
+    console.log('try again');
+}
