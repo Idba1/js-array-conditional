9bf3c78f4dec69bfb8828ad1b16ff15066dc83e3
Idba1 <moniraislamself@gmail.com>
2024-01-26 16:10:16 +0600

    pop

--- push-pop.js
+++ push-pop.js
@@ -2,3 +2,11 @@
 var friendAge = [29, 60, 45, 58, 18, 34];
 friendAge.push(67);
 console.log(friendAge);
+
+
+
+
+//Deleted elements in array
+var friendAge = [29, 60, 45, 58, 18, 34];
+friendAge.pop();
+console.log(friendAge);
