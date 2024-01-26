25ff4cf412b8a24b2a49dd16649125cf82f146de
Idba1 <moniraislamself@gmail.com>
2024-01-27 00:28:38 +0600

    ternary.js

---
+++ ternary.js
@@ -0,0 +1,8 @@
+/**
+ * Three Parts
+ * condition ? Do something when condition is true : Do something when condition is false
+ */
+
+
+var age= 23;
+(age >= 18) ? console.log('Vote dio') : console.log('Boyos hoyni')
