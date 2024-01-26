8fa1cf4af0426f5d32941f27ad4b272661451dc5
Idba1 <moniraislamself@gmail.com>
2024-01-26 06:07:46 +0600

    index of

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 //get elements by index
 var friendAge = [29, 60, 45, 58, 18, 34];
 console.log(friendAge[0]);
+
+var elements = friendAge[3];
+console.log(elements);
+
+
+//set element value by index
+friendAge[3] = 99;
+console.log(friendAge);
+
+
+//find index of an elements
+var positionIndex = friendAge.indexOf(45);
+console.log(positionIndex);
