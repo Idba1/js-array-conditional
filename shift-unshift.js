44787f0c25e1c4b5f70d1df3d28158fa01860584
Idba1 <moniraislamself@gmail.com>
2024-01-26 16:23:41 +0600

    add value in first side by using shift & unshift

---
+++ shift-unshift.js
@@ -0,0 +1,12 @@
+//Add elements in array by using UnShift
+var friendAge = [29, 60, 45, 58, 18, 34];
+friendAge.unshift(67);
+console.log(friendAge);
+
+
+
+
+//Remove elements in array by using Shift
+var friendAge = [29, 60, 45, 58, 18, 34];
+friendAge.shift();
+console.log(friendAge);
