f4704af634006b262c2d9dacd5ec4a7199e7edb7
Idba1 <moniraislamself@gmail.com>
2024-01-26 22:17:43 +0600

    update multieple condition

--- multiple-condition.js
+++ multiple-condition.js
@@ -60,3 +60,30 @@ if(isGraduated === true && (salary > 50000 || car >= 1)){
 else{
     console.log('try again');
 }
+
+
+
+
+
+const isSalary = 50000;
+const isBCS = true;
+const height = 66;
+if (salary > 100000 && isBCS === true && height > 65){
+    console.log('cole arki!');
+}
+else{
+    console.log('Ei next');
+}
+
+
+
+
+const isSalary1 = 50000;
+const isBCS1 = true;
+const height1 = 66;
+if ((isSalary1 > 100000 || isBCS1 === true) && height1 > 65){
+    console.log('cole arki!');
+}
+else{
+    console.log('Ei next');
+}
