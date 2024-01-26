0955d1037ae2062c9a96d29d7d5966b398d3f9b9
Idba1 <moniraislamself@gmail.com>
2024-01-26 22:48:42 +0600

    discount price

--- stepped.js
+++ stepped.js
@@ -1,7 +1,38 @@
-const price = 5000;
+// const price = 6000;
+// console.log(price); 
+
+// if(price >= 5000){
+//     //10% discount
+//     const discount = price * 10 / 100;
+//     console.log(discount); 
+//     const payAmmount = price - discount;
+//     console.log(payAmmount);
+// }
+// else{
+//     console.log(price);
+// }
+
+
+
+
+
+const price = 2000;
+console.log(price); 
+
 if(price >= 5000){
     //10% discount
     const discount = price * 10 / 100;
-    console.log(price); 
     console.log(discount); 
+    const payAmmount = price - discount;
+    console.log(payAmmount);
+}
+else if (price <= 2500){
+    //5% discount
+    const discount = price * 5 / 100;
+    console.log(discount);
+    const payAmmount = price - discount;
+    console.log(payAmmount); 
+}
+else{
+    console.log(price);
 }
